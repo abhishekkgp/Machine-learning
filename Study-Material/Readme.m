55ea093655384ac0e0d9782e756076a62583bf4b
Abhishek Kumar <abhisheksingnbr@gmail.com>
2021-10-08 00:06:53 +0530

    Update Readme.m

--- Study-Material/Readme.m
+++ Study-Material/Readme.m
@@ -1,3 +1,4 @@
+## 1. Random Forest
 # Method-1(better explanation): https://www.youtube.com/watch?v=v6VJ2RO66Ag&ab_channel=NormalizedNerd 
 
 # Method-2 (have two main parts): 1. https://www.youtube.com/watch?v=J4Wdy0Wc_xQ&list=PLblh5JKOoLUIE96dI3U7oxHaCAbZgfhHk&index=1&ab_channel=StatQuestwithJoshStarmer
